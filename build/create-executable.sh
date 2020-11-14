d3fcfa9a6e53f7ab19d4b7ad7f838dda8ac01d4b
Hans L'Hoëst <github@lhoest.eu>
2020-11-14 18:26:03 +0100

    fix: release not finding `cs` (#5)

--- build/create-executable.sh
+++ build/create-executable.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 sbt publishLocal
 
 # '0.+' will keep on working until 1.x is released
-curl -fLo cs https://git.io/coursier-cli-linux
-chmod +x cs
-cs bootstrap io.github.lhohan::tech-radar-from-csv:0.+ --standalone -o techradar -f
+curl -fLo cs https://git.io/coursier-cli-linux &&
+    chmod +x cs &&
+    ./cs bootstrap io.github.lhohan::tech-radar-from-csv:0.+ --standalone -o techradar -f
 
