e267d67710d4d5f8ff26e4b4b769dfe8134e09f3
hanlho <bitbucket@lhoest.eu>
2020-11-14 17:51:05 +0100

    build: logic in to locally executable scripts

---
+++ build/create-executable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euxo pipefail
+
+sbt publishLocal
+
+# '0.+' will keep on working until 1.x is released
+curl -fLo cs https://git.io/coursier-cli-linux
+chmod +x cs
+cs bootstrap io.github.lhohan::tech-radar-from-csv:0.+ --standalone -o techradar -f
+
