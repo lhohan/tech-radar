5235706ab1c700f4faf552508c6e03012197257c
hanlho <bitbucket@lhoest.eu>
2020-11-17 21:22:42 +0100

    build(sbt): Add scalastyle

--- build/build.sh
+++ build/build.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 # Run tests
 sbt test
 # Run quality checks in single step for some speed up
-sbt scalafmtCheckAll coverage test coverageReport
+sbt scalafmtCheckAll scalastyle coverage test coverageReport
 
